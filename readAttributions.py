b92030cd72c1b69eb83bf68199aa32d6d5de02aa
dominiceggerman <dominiceggerman@gmail.com>
2018-11-06 15:10:55 -0600

    Added reading function to better visualize results.

---
+++ readAttributions.py
@@ -0,0 +1,36 @@
+# By Dominic Eggerman
+# Imports
+import pandas as pd
+
+
+def readAttribs(files):
+    with open("attribResults.csv", mode="a") as out_file:
+        out_file.write("date_attr,plant_code,loc_id,R2,status\n")
+        for f in files:
+            with open(f, mode="r") as read_file:
+                for line in read_file:
+                    # Read the entry, split the data
+                    entry = line.rstrip().split("|")
+                    date = entry[3].split(":")[1].strip()
+                    plant = entry[1].split(":")[1].strip()
+                    loc_id = entry[0].split(":")[1].strip()[1:-1]
+                    if loc_id[-1] != ",":
+                        loc_id = loc_id + ","
+                    r2 = entry[2].split(":")[1].strip()
+
+                    try:
+                        if float(r2) >= 0.50:
+                            status = "good"
+                        elif float(r2) < 0.50:
+                            status = "bad"
+                    except:
+                        status = "N/A"
+
+                    # Write to file
+                    line = date + "," + plant + "," + loc_id + r2 + "," + status + "\n"
+                    out_file.write(line)
+
+
+if __name__ == "__main__":
+    files = ["confirmed_attributions.txt", "attribution_issues.txt", "database_issues.txt"]
+    readAttribs(files)
